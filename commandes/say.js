1df89379b4faacfd958cc0d48a3a85810efe9f62
Koldran <57600553+Koldran@users.noreply.github.com>
2019-11-28 21:16:44 +0100

    Add files via upload

--- commandes/say.js
+++ commandes/say.js
@@ -14,7 +14,21 @@ module.exports.run = (client, message, args) => {
 		{ return message.channel.send(':no_entry_sign: |  Vous n\'avez pas les permissions pour utiliser cette commande !').then(msg => msg.delete(5000)); }
 
 	if (!args[0]) 
-		{ return message.channel.send(':question: |  Veuiller écrire quelque chose après la commande **say**.').then(msg => msg.delete(5000)); }
+		{
+			const filter = m => m.author.id === message.author.id;
+            message.channel.send(':question: |  Que voulez-vous dire ? Veuillez écrire quelque chose. Vous pouvez faire `stop` pour annulé la commande.').then(q => q.delete(35000));
+            message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 60000
+            }).then(collected => {
+            	collected.delete(35000);
+            	message.channel.bulkDelete('2')
+            	message.channel.send(collected.first().content)
+            }).catch(err => {
+                message.channel.bulkDelete('1')
+                message.channel.send(`:x: |  Vous avez prit trop de temps pour répondre !`).then(r => r.delete(5000));
+            });
+		}
 
 	
     message.channel.send(message.content.slice(5, message.content.length));
