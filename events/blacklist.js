0d85ba743e34db4d33e5abfe3b320da57aa7a60a
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-15 20:14:03 +0100

    Update blacklist.js

--- events/blacklist.js
+++ events/blacklist.js
@@ -6,7 +6,7 @@
 module.exports = (client) => {
     let listw = ["☭", "🖕", ":middle_finger:", "卐"]
     if (message.author.bot) return;
-    if (message.guild.members.get(message.author.id).hasPermission("ADMINISTRATOR")) {
+    if (!message.guild.members.get(message.author.id).hasPermission("ADMINISTRATOR")) {
         if (message.content.includes(listw)) {
             message.delete();
         }
