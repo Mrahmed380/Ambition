cd38f65ce7982c68eab74e369e6462d6536fd7f7
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-15 17:06:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.on('messageUpdate', function (oldMessage, newMessage) {
 
 /*Invitations discord bannis*/
 client.on("message", (message) => {
-    if (!message.guild.member.get(message.author.id).hasPermission("ADMINISTRATOR")) {
+    if (!message.guild.members.get(message.author.id).hasPermission("ADMINISTRATOR")) {
         if (message.content.includes('https://discord.gg/' || 'discordapp.com/invite/')) {
             if (message.content.includes('https://discord.gg/EweFGVR', 'https://discord.gg/dpFb93r')) { }
             else { message.delete(); }
