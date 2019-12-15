3494d50981c47945710a9a228b9610821c72224f
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-15 17:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ client.on("message", (message) => {
 
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
-    if (!newMessage.member.hasPermission("ADMINISTRATOR")) {
+    if (!message.guild.members.get(message.author.id).hasPermission("ADMINISTRATOR")) {
         if (newMessage.content.includes('https://discord.gg/' || 'discordapp.com/invite/')) {
             if (newMessage.content.includes('https://discord.gg/EweFGVR', 'https://discord.gg/dpFb93r')) { }
             else { newMessage.delete(); }
