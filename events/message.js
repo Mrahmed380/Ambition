e94684b66f3cbe4062de70e7c642a10df6aa2f8a
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-18 22:23:08 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -35,7 +35,6 @@ module.exports = async (client, message) => {
   if (cmd.length === 0) return;
   let command = client.commands.get(cmd);
   if (!command) command = client.commands.get(client.aliases.get(cmd));
-  else {return;}
   if (command)
     { command.run(client, message, args); }
 };
