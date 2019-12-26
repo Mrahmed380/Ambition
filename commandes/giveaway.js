d8fa91f1c7b35dc4d866811bfbefbb8247d05bdb
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-26 18:36:35 +0100

    Add files via upload

---
+++ commandes/giveaway.js
@@ -0,0 +1,77 @@
+// @commande: clear
+// @Auteur: Koldran
+// @license Palindrome
+
+
+module.exports.run = async (bot, message, args) => {
+
+    
+    console.log(`${message.member} ran the "GiveAway" command`);
+    
+    
+    //=======================================//
+    
+
+    var item = "";
+    var time;
+    var winnerCount
+    let messageArray = message.content.split(" ");
+   
+    
+    //=======================================//
+    
+
+    for (var i = 3; i < messageArray.length; i++){
+        item += (messageArray[i] + " ");
+    }
+    winnerCount = Number(messageArray[1]);
+    time = Number(messageArray[2]);
+    
+    
+    //=======================================//
+
+
+    var giveEmbed = new Discord.RichEmbed();
+    giveEmbed.setDescription(item);
+    var embedSent = await message.channel.send(giveEmbed);
+    embedSent.react("🎉");
+    setTimeout(function() {
+        
+        
+        //=======================================//
+
+
+        var peopleReacted = embedSent.reactions.get("🎉").users;
+        var index = Math.floor(Math.random() * peopleReacted.length);
+        var winners = [];
+        var winnerMsg = "";
+        for (var i = 0; i < winners.length; i++){
+            winnerMsg += (winners[i].toString() + " ");
+        }
+        var haveHas = "has";
+        if (winners.length == 1){
+            haveHas = "has";
+        }
+        else{
+            haveHas = "have";
+        }
+        message.channel.send(winnerMsg + " " + haveHas + ` won ${item}`);
+        
+        
+        //=======================================//
+        
+        
+    }, time * 1000);
+};
+
+
+//=======================================//
+
+
+var name = 'giveaway';
+var def = name.toLowerCase();
+
+
+module.exports.help = {
+  name: def,
+};
