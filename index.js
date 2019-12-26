82e7bfff5cd131e447825cdfc351cbf022c978fc
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-26 21:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ client.on("message", async message => {
     if (message.channel.id === "593833705970073620") {
         //if (!message.member.hasPermission('ADMINISTRATOR')) {
             const links = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gmi;
-            const text = /.+/;
+            const text = /[A-Z]/gi;
             if (message.attachments.size > 0 || message.content.match(links)) {
                 if (message.attachments.size > 0 && message.content.length > 0) {
                     message.delete()
