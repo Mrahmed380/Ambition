1a3955a66efa938311787cc133357da911261896
Koldran <57600553+Koldran@users.noreply.github.com>
2019-12-26 02:19:36 +0100

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -27,11 +27,15 @@ module.exports.run = async (client, message, args) => {
 
   //=======================================//
 
+  const s1 = new Discord.RichEmbed()
+       .setColor('#2f3136')
+       .setTitle('Bienvenue sur le serveur staff, c\'est ici que vous pourrez faire vos demandes. Pour faire une demande valide, veuillez suivre le même schéma que l\'image si dessous. Si votre demande ne suit pas celui-ci, elle n\'aura pas de suite.')
+       .setImage('https://cdn.discordapp.com/attachments/645024434947620884/659563311393931275/unknown.png')
 
   const test = new Discord.RichEmbed()
       .setColor('#2f3136')
       .setTitle(':white_check_mark:')
-  message.channel.send(test).then(msg => msg.delete(5000))
+  message.channel.send(s1).then(msg => msg.delete(5000))
 
 
   //=======================================//
